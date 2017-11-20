e9491685a9bdf796956d6168dfe4d33ee4ca1ac0
Ben Haesemeyer <Benjaminhaesemeyer@gmail.com>
2017-11-20 14:53:38 -0600

    Initial commit

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+	public Rigidbody projectile;
+	public Transform shotPos;
+	public float shotForce = 1000f;
+	public float moveSpeed = 10f;
+
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis ("Horizontal") * Time.deltaTime * moveSpeed;
+		float v = Input.GetAxis ("Vertical") * Time.deltaTime * moveSpeed;
+
+		transform.Translate (new Vector3 (h, v, 0));
+
+		if (Input.GetButtonUp ("Fire1")) {
+			Rigidbody shot = Instantiate (projectile, shotPos.position, shotPos.rotation) as Rigidbody;
+			shot.AddForce(shotPos.forward * shotForce);
+		}
+		
+	}
+}
